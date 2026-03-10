24fe031a47cb769231daede9f7df19004ec742fa
unknown <xiisssiix@gmail.com>
2026-03-10 16:18:36 +0700

    edited code in js

---
+++ 06_bring-it-to-javascript.js
@@ -0,0 +1,22 @@
+let powerBank = {
+  productId: "pb-01",
+  productName: "FastCharge Power Bank 5000mAh", //String
+  price: 990,                                                                
+};
+
+let cable = {
+  productId: "cb-01",
+  productName: "USB to USB-C Cable 2M",
+  price: 250,
+};
+
+let headphone = {
+  productId: "hp-01",
+  productName: "Wireless Bluetooth Earbuds",
+  price: 1290,
+};
+
+let productCatalog = [powerBank, cable, headphone];
+
+console.log(powerBank.productName);
+console.log(productCatalog);
